39bcc8342e426338f177ed87d40b35fcd9412e7f
Sagna531 <valhaisi762@gmail.com>
2025-03-20 15:58:52 +0200

    Update script.js

--- lab 7/script.js
+++ lab 7/script.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
+    $("#addTask").click(function () {
+        let taskText = $("#taskInput").val().trim();
+        if (taskText !== "") {
+            $("#taskList").append(`<li class='list-group-item d-flex justify-content-between'>
+            ${taskText} <button class='btn btn-danger btn-sm removeTask'>Видалити</button></li>`);
+            $("#taskInput").val("");
+        }
+    });
 
+    $("#taskList").on("click", ".removeTask", function () {
+        $(this).parent().fadeOut(300, function () {
+            $(this).remove();
+        });
+    });
+});
